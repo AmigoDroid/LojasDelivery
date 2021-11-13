632094eebb92b4e7353ed83e8ad207ec21e1236e
AmigoDroid <colteman05@gmail.com>
2021-11-13 17:56:16 -0300

    comit

---
+++ client/login/index.js
@@ -0,0 +1,36 @@
+
+const form = document.getElementById('formulario');
+form.addEventListener('submit',function(e){
+ e.preventDefault();
+
+const user = document.getElementById('usuario').value;
+const pass = document.getElementById('senha').value;
+
+if(user.length<=0||user.length<=0){
+
+}else{
+    const url='https://api-server-delivery.herokuapp.com/login/loja'
+
+    const body ={
+        username:user,
+        password:pass
+    }
+    console.log(body);
+   const api = new XMLHttpRequest();
+   api.open('POST',url,true);
+   api.setRequestHeader('Content-type','application/json');
+   api.send(JSON.stringify(body));
+   api.onload=function(){
+       console.log(this.responseText);
+      res(this.responseText);
+   }
+
+}
+
+})
+function res(dados){
+
+    const db = JSON.parse(dados);
+    localStorage.setItem('tokenLoja',db.token);
+    location.replace('../home/index.html');
+}
