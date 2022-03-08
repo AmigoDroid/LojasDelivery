28ca65072a9490f9b28345b7df0a061badbd8d4a
AmigoDroid <canalmeuovo36@gmail.com>
2022-03-08 13:31:18 -0300

    update

--- client/login/index.js
+++ client/login/index.js
@@ -1,40 +1,26 @@
 
-const form = document.getElementById('formulario');
-form.addEventListener('submit',function(e){
- e.preventDefault();
 
-const user = document.getElementById('usuario').value;
-const pass = document.getElementById('senha').value;
+//     console.log(body);
+//    const api = new XMLHttpRequest();
+//    api.open('POST',url,true);
+//    api.setRequestHeader('Content-type','application/json');
+//    api.send(JSON.stringify(body));
+//    api.onload=function(){
+//        console.log(this.responseText);
+//       res(this.responseText);
+//    }
 
-if(user.length<=0||user.length<=0){
+// }
 
-}else{
-    const url='https://api-server-delivery.herokuapp.com/login/loja'
-    const body ={
-        username:user,
-        password:pass
-    }
-    console.log(body);
-   const api = new XMLHttpRequest();
-   api.open('POST',url,true);
-   api.setRequestHeader('Content-type','application/json');
-   api.send(JSON.stringify(body));
-   api.onload=function(){
-       console.log(this.responseText);
-      res(this.responseText);
-   }
+// })
+// function res(dados){
 
-}
-
-})
-function res(dados){
-
-    const db = JSON.parse(dados);
+//     const db = JSON.parse(dados);
     
-    if(db.status==true){
-    localStorage.setItem('tokenLoja',db.token);
-    localStorage.setItem('idLoja',db.idLoja);
-    location.replace('../home/index.html');
-}else{
-alert('usuario ou senha incorretos');
-}}
+//     if(db.status==true){
+//     localStorage.setItem('tokenLoja',db.token);
+//     localStorage.setItem('idLoja',db.idLoja);
+    
+// }else{
+// alert('usuario ou senha incorretos');
+// }}
